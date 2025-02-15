4ca426813d88e94199ee06f0559e5d163cbbe8d0
rifki504 <rifkinurohmat66@gmail.com>
2025-02-15 09:40:09 +0700

    first commit

---
+++ kuliah/pertemuan2/latihan2.php
@@ -0,0 +1,10 @@
+<?php
+    $nama = "Rifkinrmdni";
+    $email = "Rifkinrmdni@gmail.com";
+    $pekerjaan = "Mahasiswa";
+    $jurusan = "Teknik Informatika";
+    $universitas = "UNPAS";
+    echo "<h1>$nama</h1>";
+    echo "<h4>$email</h4>";
+    echo "<p>Hallo, nama saya $nama. saya adalah seorang $pekerjaan, $jurusan, di $universitas.</p>";
+?>
---
+++ tugas/tugas1/1b.php
@@ -0,0 +1,18 @@
+<?php
+// Inisialisasi variabel dengan 2 digit terakhir NPM
+$angka = 54; // Ganti dengan 2 digit terakhir NPM Anda
+
+echo "Aku adalah angka <b>$angka</b><br>";
+
+$angka *= 5;
+echo "Jika aku dikali 5, maka aku sekarang menjadi <b>$angka</b><br>";
+
+$angka /= 2;
+echo "Jika aku dibagi 2, maka aku sekarang menjadi <b>$angka</b><br>";
+
+$angka += 75;
+echo "Jika aku ditambah 75, maka aku sekarang menjadi <b>$angka</b><br>";
+
+$angka -= 20;
+echo "Jika aku dikurangi 20, maka aku sekarang menjadi <b>$angka</b><br>";
+?>
